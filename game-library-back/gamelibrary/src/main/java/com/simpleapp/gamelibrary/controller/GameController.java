5ad917a1f0075c9eee2e4c84d678002ca290a5a0
IvanIsaev-12 <vanya.isaev.442@gmail.com>
2025-07-16 16:42:44 +0200

    Bug fixes. Get games of the current user

--- game-library-back/gamelibrary/src/main/java/com/simpleapp/gamelibrary/controller/GameController.java
+++ game-library-back/gamelibrary/src/main/java/com/simpleapp/gamelibrary/controller/GameController.java
@@ -2,9 +2,12 @@ package com.simpleapp.gamelibrary.controller;
 
 
 import com.simpleapp.gamelibrary.entity.Game;
+import com.simpleapp.gamelibrary.entity.User;
 import com.simpleapp.gamelibrary.service.GameService;
+import com.simpleapp.gamelibrary.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +20,12 @@ import java.util.List;
 public class GameController {
 
     private final GameService gameService;
+    private final UserService userService;
 
-    @GetMapping()
+    @GetMapping("/my-library")
     public ResponseEntity<List<Game>> getGames() {
-        System.out.println("GET /api/games called");
-        List<Game> games = gameService.getAllGames();
-        System.out.println("Fetched games: " + games.size());
+        User currentUser = userService.getCurrentUser();
+        List<Game> games = gameService.getAllGames(currentUser);
         return ResponseEntity.ok(games);
     }
 
